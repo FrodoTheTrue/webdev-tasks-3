968eca37722b46d531cd8bdc6c988753bbcdf228
Fedor <mydearfrodo@yandex.ru>
2016-03-17 11:47:46 +0500

    fix()

--- tests/flow-test.js
+++ tests/flow-test.js
@@ -8,28 +8,48 @@ var fs = require('fs');
 var directory = './cats';
 
 var fun1 = function(next) {
-    setTimeout('second', 100);
-    next(null, 'result1');
+    setTimeout(function() {
+        next(null, 'result1');
+    }, 500);
 };
 var funParallel = function(next) {
-    next(null, 'result2');
+    setTimeout(function() {
+        next(null, 'result2');
+    }, 500);
 };
 var fun2 = function(data, next) {
-    next(null, 'result2');
+    setTimeout(function() {
+        next(null, 'result2');
+    }, 500);
 };
 var fun3 = function(data, next) {
-    next(null, 'result3');
+    setTimeout(function() {
+        next(null, 'result3');
+    }, 500);
 };
 var fun4 = function(data, next) {
-    next(null, 'result4');
+    setTimeout(function() {
+        next(null, 'result4');
+    }, 500);
 };
 var funError = function(data, next) {
     next(true, 'result');
 };
+
+var funErrorParallel = function(next) {
+    next(true, 'result');
+};
+
 var mapFunction = function(value, next) {
     next(null, value * 2);
 };
+var mapFunctionError = function(value, next) {
+    next(true, value * 2);
+};
 
+var myCallback = function(error, data) {
+
+};
 describe('flow.serial', function () {
     describe('result callback', function() {
         it('should be called once', function() {
@@ -39,24 +59,34 @@ describe('flow.serial', function () {
         });
 
         it('should take 2 arguments', function() {
-            var spy = sinon.spy();
+            var spy = sinon.spy(function(err, data) {
+                var resultCallback = spy.getCall(0);
+                resultCallback.args.length.should.equal(2);
+            });
             flow.serial([fun1, fun2, fun3], spy);
-            var resultCallback = spy.getCall(0);
-            resultCallback.args.length.should.equal(2);
         });
         it('should have data of the last function', function() {
-            var spy = sinon.spy();
+            var spy = sinon.spy(function(error, data) {
+                var resultCallback = spy.getCall(0);
+                resultCallback.args[1].should.equal('result3');
+            });
             flow.serial([fun1, fun2, fun3], spy);
-            var resultCallback = spy.getCall(0);
-            resultCallback.args[1].should.equal('result3');
+        });
+        it('should have error in results callback if error was in functions', function() {
+            var spy = sinon.spy(function(err, data){
+                var resultCallback = spy.getCall(0);
+                resultCallback.args[0].should.be.equal(true);
+            });
+            flow.serial([fun1, funError], spy);
         });
     });
     it('should send functions data serial', function() {
-        var spyFun4 = sinon.spy(fun4);
-        var spy = sinon.spy();
+        var spyFun4 = sinon.spy(function(data, next) {
+            var resultCallback = spyFun4.getCall(0);
+            resultCallback.args[0].should.equal('result3');
+            next(null, 'result4');
+        });
         flow.serial([fun1, fun2, fun3, spyFun4], function(err, data) {});
-        var resultCallback = spyFun4.getCall(0);
-        resultCallback.args[0].should.equal('result3');
     });
     it('should not call next function after error', function() {
         var spyFun3 = sinon.spy(fun3);
@@ -88,6 +118,13 @@ describe('flow.parallel', function () {
             });
             flow.parallel([fun1, funParallel], spy);
         });
+        it('should have error in results callback if error was in functions', function() {
+            var spy = sinon.spy(function(err, data){
+                var resultCallback = spy.getCall(0);
+                resultCallback.args[0].should.be.equal(true);
+            });
+            flow.parallel([fun1, funErrorParallel], spy);
+        });
     });
 });
 
@@ -113,6 +150,13 @@ describe('flow.map', function () {
                 resultCallback.args[1].should.deep.equal([4, 8]);
             });
             flow.map([2, 4], mapFunction, spy);
+        });
+        it('should have error in results callback if error was in function calls', function() {
+            var spy = sinon.spy(function(err, data){
+                var resultCallback = spy.getCall(0);
+                resultCallback.args[0].should.be.equal(true);
+            });
+            flow.map([2, 4], mapFunctionError, spy);
         });
     });
 });
